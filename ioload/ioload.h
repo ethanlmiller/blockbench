ec14f971a3fc69c9c65096aed13db27a118b3b42
Ethan L. Miller <elm@cs.ucsc.edu>
2013-08-14 10:34:43 -0700

    Added copyright notice to source files.
    Added ioload.h to the repo - for some reason, wasn't previously added.

--- ioload/ioload.h
+++ ioload/ioload.h
@@ -1,5 +1,5 @@
 //
-// iofill.h
+// ioload.h
 //
 //------------------------------------------------------------------------------------------
 // Copyright (c) 2013, Pure Storage, Inc.
@@ -35,15 +35,8 @@
 #define	NLATBKTS		100000
 #define	NSEC			20000
 #define	LATDIV			10000
-#define	IOB_MAXSEG		4
 #define	STATIC			static
-
-/* Methods of flushing a device cache*/
-
-#define	SYNC_CACHE_16		0
-#define	SYNC_CACHE_10		1
-#define	SATA_STANDBY_IMM	2
-#define	SATA_FLUSH_CACHE	3
+#define	NIOTRACE		100000
 
 #define	_GNU_SOURCE	1
 
@@ -101,145 +94,93 @@ struct ioreq {
 	char		*readbp;
 	char		*writebp;
 	struct iothread	*threadp;
+	struct itype	*itype;
 };
 
 struct cmdparams {
-	double		readpct;
-	uint32_t	randseed;
-	int		seqflag;
-	int		size;
-	int		altsize;
-	uint64_t	altcount;
-	uint64_t	nio;
-	uint64_t	nrep;
-	uint64_t	seekoff;
-	uint64_t	headersz;
-	int		skip;
-	int		nthreads;
-	int		streams;
-	uint64_t	wroff;
 	uint64_t	minoff;
 	uint64_t	maxoff;
+	uint64_t	headersz;
+	uint64_t	warmup;
+	uint64_t	interval;
+	uint32_t	maxlatency;
+	uint32_t	maxrate;
+	uint32_t	status;
 	int		multiplier;
-	int		partition;
-	int		openflags;
-	int		writeinject;
-	int		writesize;
-	int		segsize;
-	int		nsegments;
-	int		nsubseg;
-	int		randinject;
-	int		threadalign;
-	int		flush;
-	int		flushcount;
-	int		flushtype;
-	int		preflushtype;
-	int		postflushtype;
-	int		flushalign;
-	uint64_t	flushinterval;
-	uint64_t	preflushinterval;
-	uint64_t	postflushinterval;
-	uint64_t	injectinterval;
-	uint64_t	injectinterval_io;
-	uint64_t	iodetcnt;
-	int		uniform;
-	uint64_t	iodettime;
-	uint64_t	nsleeptime;
-	uint64_t	recoverytime;
-	uint64_t	maxtime;
-	int		dynamicrecovery;
-	int		datatype;
+	int		initthreads;
+	int		maxthreads;
+	int		incrthreads;
+	int		maxiosize;
+};
+
+struct itype {
+	struct itype	*next;
+	char		*buf;
+	double		pct;
 	uint64_t	dedupblks;
+	int		optype;
+	uint32_t	size;
+	int		dtype;
+	uint64_t	longlat;
+	double		repwrite;
+};
+
+struct iostats {
+	uint64_t		latbuckets[NLATBKTS][N_OPTYPES];
+	uint64_t		totreads;
+	uint64_t		totwrites;
+	uint64_t		totlat[N_OPTYPES];
+	uint64_t		totbytes[N_OPTYPES];
 };
 
 struct iothread {
 	struct iothread		*next;
-	struct fthread		*pthread;
+	struct file		*file;
 	struct ioreq		*iorp;
 	iob_sema_t		start;
 	iob_sema_t		stop;
-	iob_sema_t		iostart;
-	iob_sema_t		iostop;
 	struct cmdparams	params;
+	struct itype		*itlist;
 	int			threadid;
-	int			threadalign;
 	int			fd;
-	uint64_t		*shared_area;
-	uint64_t		iocount;
-	uint64_t		nextbno;
-	struct iothread		*streamhead;
-	char			*buf;
-	char			*writebuf;
-	uint64_t		cursegoff[IOB_MAXSEG];
-	int			segwrite[IOB_MAXSEG];
-	int			nsegoffs;
-	int			*segofflist;
-	int			writenum;
-	int			flushnum;
-	int			pflushnum;
-	int			recovered;
-	int			rec_runlen;
-	uint64_t		latbuckets[NLATBKTS][N_STATTYPES];
-	uint64_t		totreads;
-	uint64_t		totwrites;
-	uint64_t		totflushes;
-	uint64_t		totpflushes;
-	uint64_t		totlat[N_STATTYPES];
-	uint64_t		totbytes[N_STATTYPES];
-	int			seconds[NSEC];
-	int			nseconds;
-	uint64_t		nextsec;
-	uint64_t		starttime;
-	uint64_t		stoptime;
 	uint64_t		maxblks;
 	uint64_t		minblks;
 	uint64_t		rangeoff;
-	uint64_t		maxsegs;
+	struct iostats		cur;
 };
 
-struct fthread {
-	struct fthread		*next;
+struct file {
+	struct file		*next;
 	struct iothread		*ithreads;
-	iob_sema_t		start;
-	iob_sema_t		stop;
-	struct cmdparams	params;
 	char			*filename;
 	int			fileno;
-	int			iowait;
-	uint64_t		*shared_block;
-	uint64_t		thread_bno;
-	uint64_t		totalio;
-	uint64_t		latbuckets[NLATBKTS][N_STATTYPES];
-	uint64_t		totreads;
-	uint64_t		totwrites;
-	uint64_t		totflushes;
-	uint64_t		totpflushes;
-	uint64_t		starttime;
-	uint64_t		stoptime;
-	uint64_t		endtime;
-	uint64_t		totlat[N_STATTYPES];
-	uint64_t		totbytes[N_STATTYPES];
+	struct cmdparams	params;
+	struct itype		*itlist;
+	struct iostats		cur;
+	struct iostats		use;
+	struct iostats		last;
+};
+
+struct curtest {
+	int		threads;
+	uint64_t	endwarmup;
+	uint64_t	endinterval;
+	uint64_t	finish;
 };
 
 #define	IOB_CACHE_LINE		(128 / sizeof (uint64_t))
 
-#define	FLUSH_OFF		0
-#define	FLUSH_AFT		1
-#define	FLUSH_BEF		2
-#define	FLUSH_SEG		3
-#define	FLUSH_ONLY		4
-
-#define	PFLUSH_SLEEP		1
-#define	PFLUSH_READ		2
-#define	PFLUSH_MIXED		3
-
 void		runtest(void);
-void		output(uint64_t start, uint64_t stop, struct rusage *brp);
-void		init_threads(int nfiles, char *filenames[], struct cmdparams *cp);
+void		output(uint64_t start, uint64_t stop, struct rusage *brp, int status);
+void		init_threads(int nfiles, char *filenames[], struct cmdparams *cp, struct itype *itlist);
 
-extern struct fthread	*flist;
+extern struct file	*flist;
+extern struct curtest	*current;
 extern char		*outputfile;
-extern uint64_t		statusmb;
+extern int		verbose;
+extern int		lat_details;
+extern int		longlats;
+extern int		ign_errors;
 
 #if defined(__CYGWIN__)
 extern ssize_t	cyg_read(int fd, char *buf, uint32_t length, uint64_t offset);
